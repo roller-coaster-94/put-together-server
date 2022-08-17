95f2cfadc0a9980538250829155b4eb9e7a5bd23
jhkang1517 <jhkang1517@gmail.com>
2022-08-17 22:19:44 +0900

    init setting : spring initializr
    Signed-off-by: jhkang1517 <jhkang1517@gmail.com>

---
+++ src/main/java/kr/co/puttogether/PuttogetherApplication.java
@@ -0,0 +1,13 @@
+package kr.co.puttogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuttogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuttogetherApplication.class, args);
+	}
+
+}
