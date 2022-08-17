4aed386b6ac0b9e874fb252ef8a315f38ed7dc1a
jhkang1517 <jhkang1517@gmail.com>
2022-08-17 22:53:59 +0900

    init setting : project package structure
    Signed-off-by: jhkang1517 <jhkang1517@gmail.com>

--- src/test/java/kr/co/puttogether/PutTogetherApplicationTests.java
+++ src/test/java/kr/co/puttogether/PutTogetherApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PuttogetherApplicationTests {
+class PutTogetherApplicationTests {
 
 	@Test
 	void contextLoads() {
