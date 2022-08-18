ea6b6b6d3f12d890646a7d5fbfc492c8a72529a5
jhkang1517 <jhkang1517@gmail.com>
2022-08-18 21:33:16 +0900

    init setting : spring initializr
    Signed-off-by: jhkang1517 <jhkang1517@gmail.com>

---
+++ src/test/java/kr/co/puttogether/PuttogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.puttogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuttogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
